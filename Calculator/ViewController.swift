2ea71cf9fe9bf60781e8853ef2ed406e148259a0
Brexton <brextonpham@stanford.edu>
2015-09-21 17:18:28 -0700

    Uploading Calculator

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func touchDigit(sender: UIButton) {
+        let digit = sender.currentTitle
+        print("touchDigit was sent")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
